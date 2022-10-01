cc9d2ce498fe5a9adcba58a06917c471f6dd053b
Jayden <jaydendare2001@gmail.com>
2022-10-01 22:17:15 +0100

    starting commit

---
+++ src/Exercise.java
@@ -0,0 +1,26 @@
+public class Exercise {
+    private String exerciseName;
+    private int repCount;
+    private int weight;
+
+    public String getExerciseName() {
+        return exerciseName;
+    }
+
+    public void setExerciseName(String exerciseName) {
+        this.exerciseName = exerciseName;
+    }
+
+    public void setRepCount(int repCount) {
+        this.repCount = repCount;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+}
